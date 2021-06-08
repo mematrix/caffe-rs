ff81ff1c77693ee31ef093738bae89ac80066c04
mematrix <mematrix@users.noreply.github.com>
2021-06-09 01:51:19 +0800

    Impl the `bias_layer.rs`.

--- src/layers/bias_layer.rs
+++ src/layers/bias_layer.rs
@@ -1,7 +1,12 @@
+use std::rc::Rc;
+
+use cblas::Transpose;
+
 use crate::blob::{BlobType, Blob};
+use crate::filler::get_filler;
 use crate::layer::{LayerImpl, CaffeLayer, BlobVec, def_layer_setup, make_shared_blob};
 use crate::proto::caffe::LayerParameter;
-use crate::filler::get_filler;
+use crate::util::math_functions::{caffe_set, caffe_copy};
 
 
 pub struct BiasLayer<T: BlobType> {
@@ -69,7 +74,40 @@ impl<T: BlobType> CaffeLayer for BiasLayer<T> {
     }
 
     fn reshape(&mut self, bottom: &BlobVec<Self::DataType>, top: &BlobVec<Self::DataType>) {
-        todo!()
+        let param = self.layer.layer_param.get_bias_param();
+        let b1 = bottom.get(1).map(|b| b.as_ref().borrow());
+        let blobs = &self.layer.blobs;
+        let bias = b1.unwrap_or_else(|| blobs[0].as_ref().borrow());
+        // Always set axis == 0 in special case where bias is a scalar
+        // (num_axes == 0). Mathematically equivalent for any choice of axis, so the
+        // actual setting can be safely ignored; and computation is most efficient
+        // with axis == 0 and (therefore) outer_dim_ == 1.
+        let b0 = bottom[0].as_ref().borrow();
+        let axis = if bias.num_axes() == 0 {
+            0
+        } else {
+            b0.canonical_axis_index(param.get_axis()) as i32
+        };
+        assert!(b0.num_axes() >= axis + bias.num_axes(),
+                "bias blob's shape extends past bottom[0]'s shape when applied starting with bottom[0] axis = {}",
+                axis);
+        for i in 0..bias.num_axes() {
+            assert_eq!(b0.shape_idx(axis + i), bias.shape_idx(i),
+                       "dimension mismatch between bottom[0]->shape({}) and bias->shape({})",
+                       axis + i, i);
+        }
+
+        self.outer_dim = b0.count_range(0, axis as usize);
+        self.bias_dim = bias.count() as i32;
+        self.inner_dim = b0.count_range_to_end((axis + bias.num_axes()) as usize);
+        self.dim = self.bias_dim * self.inner_dim;
+        if !Rc::ptr_eq(&bottom[0], &top[0]) {
+            top[0].borrow_mut().reshape_like(&*bottom[0].as_ref().borrow());
+        }
+        self.bias_multiplier.reshape(&[self.inner_dim]);
+        if self.bias_multiplier.cpu_data()[self.inner_dim as usize - 1usize] != T::from_i32(1) {
+            caffe_set(self.inner_dim as usize, T::from_i32(1), self.bias_multiplier.mutable_cpu_data());
+        }
     }
 
     fn layer_type(&self) -> &'static str {
@@ -89,7 +127,25 @@ impl<T: BlobType> CaffeLayer for BiasLayer<T> {
     }
 
     fn forward_cpu(&mut self, bottom: &BlobVec<Self::DataType>, top: &BlobVec<Self::DataType>) {
-        todo!()
+        let b1 = bottom.get(1).map(|b| b.as_ref().borrow());
+        let blobs = &self.layer.blobs;
+        let bias = b1.unwrap_or_else(|| blobs[0].as_ref().borrow());
+        let bias_data = bias.cpu_data();
+        let mut t0 = top[0].borrow_mut();
+        let top_data = t0.mutable_cpu_data();
+        if !Rc::ptr_eq(&bottom[0], &top[0]) {
+            let b0 = bottom[0].as_ref().borrow();
+            let bottom_data = b0.cpu_data();
+            caffe_copy(b0.count(), bottom_data, top_data);
+        }
+
+        let mut top_offset = 0usize;
+        for n in 0..self.outer_dim {
+            T::caffe_cpu_gemm(Transpose::None, Transpose::None, self.bias_dim, self.inner_dim, 1,
+                              T::from_i32(1), bias_data, self.bias_multiplier.cpu_data(),
+                              T::from_i32(1), &mut top_data[top_offset..]);
+            top_offset += self.dim as usize;
+        }
     }
 
     fn forward_gpu(&mut self, _bottom: &BlobVec<Self::DataType>, _top: &BlobVec<Self::DataType>) {
@@ -97,7 +153,33 @@ impl<T: BlobType> CaffeLayer for BiasLayer<T> {
     }
 
     fn backward_cpu(&mut self, top: &BlobVec<Self::DataType>, propagate_down: &Vec<bool>, bottom: &BlobVec<Self::DataType>) {
-        todo!()
+        if propagate_down[0] && !Rc::ptr_eq(&bottom[0], &top[0]) {
+            let t0 = top[0].as_ref().borrow();
+            let mut b0 = bottom[0].borrow_mut();
+            let count = b0.count();
+            caffe_copy(count, t0.cpu_diff(), b0.mutable_cpu_diff());
+        }
+
+        // in-place, we don't need to do anything with the data diff
+        let bias_param = bottom.len() == 1;
+        if (!bias_param && propagate_down[1]) || (bias_param && self.layer.param_propagate_down[0]) {
+            let t0 = top[0].as_ref().borrow();
+            let b1 = bottom.get(1).map(|b| b.borrow_mut());
+            let blobs = &self.layer.blobs;
+            let mut bias = b1.unwrap_or_else(|| blobs[0].borrow_mut());
+
+            let top_diff = t0.cpu_diff();
+            let bias_diff = bias.mutable_cpu_diff();
+            let mut accum = bias_param;
+            let mut top_offset = 0usize;
+            for n in 0..self.outer_dim {
+                T::caffe_cpu_gemv(Transpose::None, self.bias_dim, self.inner_dim, T::from_i32(1),
+                                  &top_diff[top_offset..], self.bias_multiplier.cpu_data(),
+                                  T::from_i32(accum as i32), bias_diff);
+                top_offset += self.dim as usize;
+                accum = true;
+            }
+        }
     }
 
     fn backward_gpu(&mut self, _top: &BlobVec<Self::DataType>, _propagate_down: &Vec<bool>, _bottom: &BlobVec<Self::DataType>) {
